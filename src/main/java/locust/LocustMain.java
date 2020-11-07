5cb6887cc21c8417d322b74bf3088a284ce71578
Carise Fernandez <carise@gmail.com>
2020-11-06 16:46:45 -0800

    Simple args

---
+++ src/main/java/locust/LocustMain.java
@@ -0,0 +1,12 @@
+package locust;
+
+public class LocustMain {
+
+  public static void main(String[] args) {
+    // Arg parsing for -i and -o
+    String input = args[0];
+    String inputFilename = args[1];
+    String output = args[2];
+    String outputFilename = args[3];
+  }
+}
